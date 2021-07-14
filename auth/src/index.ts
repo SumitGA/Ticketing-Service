90ed64fd5e96cdffd7ce49655956a83c5748ba5a
Sumit Gautam <sghost33@gmail.com>
2021-07-14 22:10:29 +1000

    Add express validator for request validation in express for authentication service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
-import { json } from 'express';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { siginoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 const app = express();
-app.use(json);
+app.use(express.json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there');
-})
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(siginoutRouter);
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!')
+  console.log('Listening on port 3000')
 })
