3ee8b89a6ea3534d78c610ab977af3b9d2f4229e
Sumit Gautam <sghost33@gmail.com>
2021-07-15 06:46:25 +1000

    Add CustomError abstract to throw out specific error to the frontend

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,9 +1,10 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
   constructor(public errors: ValidationError[]) {
-    super();
+    super('Invalid inputs');
 
     // Only because we are extending built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
