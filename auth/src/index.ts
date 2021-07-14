5e50586a0eab85707172b1dd9485fe3d8ec65b2f
Sumit Gautam <sghost33@gmail.com>
2021-07-15 06:22:04 +1000

    Adding custom error handler middleware and sub classing different error exceptions

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,15 +3,18 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { siginoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { json } from 'body-parser';
 
 const app = express();
-app.use(express.json());
+app.use(json());
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(siginoutRouter);
 
+app.use(errorHandler);
 app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
@@ -12,9 +14,17 @@ router.post('/api/users/signup', [
     .isLength({ min: 4, max: 20 })
     .withMessage('Password must be between 4 and 20 characters')
 ], (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array());
+  }
+
   const { email, password } = req.body;
 
-  // new User({email, password})
+  res.send({});
+  console.log('Creating a user...')
+  throw new DatabaseConnectionError();
 })
 
 export { router as signupRouter }
