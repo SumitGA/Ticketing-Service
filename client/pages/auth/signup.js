0dc73eaf02b94076702db8a1907f75eb1f7a64cf
Sumit Gautam <sghost33@gmail.com>
2021-07-18 09:07:50 +1000

    Sign up page request to auth service and redirecting user to landing page

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,25 @@
 import { useState } from 'react';
+import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
 
 const SignUp = () => {
 	const [ email, setEmail ] = useState('');
 	const [ password, setPassword ] = useState('');
+	const { doRequest, errors } = useRequest({
+		url: '/api/users/signup',
+		method: 'post',
+		body: {
+			email,
+			password
+		},
+		onSuccess: () => Router.push('/')
+	});
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    console.log(email, password);
-  }
-  
+	const onSubmit = async (event) => {
+		event.preventDefault();
+		await doRequest();
+	};
+
 	return (
 		<form onSubmit={onSubmit}>
 			<h1>Sign Up</h1>
@@ -20,6 +31,7 @@ const SignUp = () => {
 				<label>Password</label>
 				<input value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" />
 			</div>
+			{errors}
 			<button className="btn btn-primary">Sign Up</button>
 		</form>
 	);
