1349e24b0f0425aadf7200de758e295e960c0af5
Sumit Gautam <sghost33@gmail.com>
2021-07-18 15:53:15 +1000

    Moving getInitialProps to _app component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-const App = ({Component, pageProps}) => {
-  return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps }) => {
+	return (
+		<div>
+			<h1>Header!</h1>
+			<Component {...pageProps} />
+		</div>
+	);
 };
 
-export default App;
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+  console.log(data);
+  return data;
+};
+
+export default AppComponent;
