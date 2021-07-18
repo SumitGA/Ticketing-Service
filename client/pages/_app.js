2b81150e98513c5822dae33e078180f8aed28db3
Sumit Gautam <sghost33@gmail.com>
2021-07-18 16:05:50 +1000

    Information fetching by accessing getIntialProps from all the pages and passing them as props

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,20 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from '../api/build-client';
 
-const AppComponent = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
 	return (
 		<div>
-			<h1>Header!</h1>
+			<h1>Header! {currentUser && currentUser.email} </h1>
 			<Component {...pageProps} />
 		</div>
 	);
 };
 
 AppComponent.getInitialProps = async (appContext) => {
-  const client = buildClient(appContext.ctx);
-  const { data } = await client.get('/api/users/currentuser');
-  console.log(data);
-  return data;
+	const client = buildClient(appContext.ctx);
+	const { data } = await client.get('/api/users/currentuser');
+
+	// Calling getInitialProps of Landing page and manually sending back data to the Landing page
+	let pageProps = {};
+	if (appContext.Component.getInitialProps) {
+		pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+	}
+  //Returning data and props to all the pages
+	return {
+		pageProps,
+		...data
+	};
 };
 
 export default AppComponent;
