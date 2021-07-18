7a1b4dd03c918bc93d4f78aedab2e27f651f101b
Sumit Gautam <sghost33@gmail.com>
2021-07-18 15:16:00 +1000

    Add sending cookie along with header to ingress-nginx-controller from different service in kube cluster

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,25 @@
-const LandingPage = () => {
-  return (
-    <div>Landing Page</div>
-  )
-}
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+	return <div>Landing Page</div>;
+};
+
+LandingPage.getInitialProps = async ({ req }) => {
+	if (typeof window === 'undefined') {
+		const {
+			data
+		} = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+			headers: req.headers
+		});
+		return data;
+		// we are on the server
+	} else {
+		// we are on the browser!
+		// requests can be made with a base url of ''
+		const { data } = await axios.get('/api/users/currentuser');
+		return data;
+	}
+	return {};
+};
+
 export default LandingPage;
