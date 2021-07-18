5ce4e84d0b8e279659e5f587963ffc8bb7bbecfe
Sumit Gautam <sghost33@gmail.com>
2021-07-18 15:35:19 +1000

    Add signin page and functionality

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,7 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-	console.log(currentUser);
-	return <div>Landing Page</div>;
+	 return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>
 };
 
 LandingPage.getInitialProps = async (context) => {
