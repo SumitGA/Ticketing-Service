949cd7e41b9c9aca46308658777731c5deb78015
Sumit Gautam <sghost33@gmail.com>
2021-10-21 11:22:40 +1100

    More on creating generic listener using typescript typechecking on data types

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,12 +1,17 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
+import { TicketCreatedEvent } from './ticket-created-events';
+import { Subjects } from './subjects';
 
-export class TickeCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TickeCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('Event data!', data);
+    console.log(data.id);
+    console.log(data.price);
+    console.log(data.title);
     msg.ack()
   }
 }
