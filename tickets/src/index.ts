bac083c22f4eef0cae443cdbe61afd0f9ff9135c
Sumit Gautam <sghost33@gmail.com>
2021-10-21 19:28:57 +1100

    Setting up nats env variables and finishing ticketing service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -10,8 +10,21 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error('Mongo URI must be defined')
   }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined')
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined')
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined')
+  }
   try {
-    await natsWrapper.connect('ticketing', 'asdfkl', 'http://nats-srv:4222');
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     //Closing nats connection when the server terminiates
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
