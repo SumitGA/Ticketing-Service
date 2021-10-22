5c4625d91b80cb6a122518dd809aec64113704d3
Sumit Gautam <sghost33@gmail.com>
2021-10-22 17:38:24 +1100

    Fix routes for order services

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,7 +3,10 @@ import 'express-async-errors';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@sumitga-tickets/common';
 import { json } from 'body-parser';
-
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes/index';
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,6 +16,11 @@ app.use(cookieSession({
   secure: process.env.NODE_ENV !== 'test'
 }))
 app.use(currentUser);
+
+app.use(indexOrderRouter);
+app.use(showOrderRouter);
+app.use(newOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all('*', async (req, res, next) => {
   throw new NotFoundError();
