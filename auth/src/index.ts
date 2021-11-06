9f66f7fadc93e8d5a9686e772988a1eec616908d
Sumit Gautam <sghost33@gmail.com>
2021-11-06 22:20:15 +1100

    Startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import { app } from './app';
 
 // Connecting to mongodb instance from the kube pod
 const start = async () => {
+  console.log('Starting up the server ......')
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
